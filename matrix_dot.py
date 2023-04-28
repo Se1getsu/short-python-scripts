c0c2116a7dc9991e0e7f5b5d715ad8b36b2d0f4d
霽月 <102911089+Se1getsu@users.noreply.github.com>
2023-04-28 10:04:39 +0900

    refactor: 少し気になるところを修正

--- matrix_dot.py
+++ matrix_dot.py
@@ -1,4 +1,4 @@
-﻿# 【内容】
+# 【内容】
 # 行列の内積を計算するコードです。
 # 1年生が作ったコードが酷かったので、私なりに作り直してみました。
 
@@ -10,16 +10,15 @@ def input_matrix(name):
         inp = input('%s>> ' % name)
         if inp == '': break
         new_row = inp.split(' ')
-        listX.append(new_row)
-        if len(listX[-1]) != len(listX[0]):
+        if len(new_row) != len(listX[0]):
             raise ValueError()
+        listX.append(new_row)
     
     return np.array(listX, dtype=int)
 
 try:
     a = input_matrix('A')
-    b = input_matrix('B'); print()
-
+    b = input_matrix('B')
     print(np.dot(a,b))
     
 except Exception:
